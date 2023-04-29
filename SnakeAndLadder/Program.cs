84d1e52ff682cc38b546a21fd29610b08615d881
Rak65 <rakeshp6505@gmail.com>
2023-04-29 22:23:54 +0530

    [200~checks for

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,7 +11,7 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Snake & Ladder Simulator");
-            SnakeAndLadder.DiceRoll();
+            SnakeAndLadder.ChecksNoplayLadderSnake();
             Console .ReadKey();
         }
     }
--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,18 +8,36 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadder
     {
-        public static void DiceRoll()
+        public static void ChecksNoplayLadderSnake()
         {
             Console.WriteLine("start the snake and ladder Game");
-            Console.WriteLine("Press any key to roll the dice");
-            Console.ReadKey();
+            int position = 0;
+                // generate a random number between 1 and 3 to determine the option
+                Random random = new Random();
+                int option = random.Next(1, 4);
 
-            // generate a random number between 1 and 6 after dice roll
-            Random random = new Random();
-            int rollDice = random.Next(1, 7);  //used random to get the number between 1 to 6
+                switch (option)
+                {
+                    case 1: // No Play
+                        Console.WriteLine("No Play --> You stay in the same position");
+                        break;
+                    case 2: // Ladder
+                        int ladder = random.Next(1, 7); // generate a random number for the ladder position
+                        Console.WriteLine("Ladder --> You move ahead by " + ladder + " positions");
+                        position += ladder; // update position + random ladder
+                        break;
+                    case 3: // Snake
+                        int snake = random.Next(1, 7); // generate a random number for the snake position
+                        Console.WriteLine("Snake --> You move back by " + snake + " positions");
+                        position -= snake; // update position - random number
+                        if (position < 0) 
+                        {
+                            position = 0; // set position to 0.
+                        }
+                        break;
+                }
+            Console.WriteLine("You are now at position " + position);
 
-            Console.WriteLine();
-            Console.WriteLine("You rolled a " + rollDice);
         }
     }
         
