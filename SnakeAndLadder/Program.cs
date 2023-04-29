6e1ccc41bfa0d3092a520e6b524441749df97e41
Rak65 <rakeshp6505@gmail.com>
2023-04-29 21:23:05 +0530

    Roll the Dice

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,7 +11,7 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Snake & Ladder Simulator");
-            SnakeAndLadder.Call();
+            SnakeAndLadder.DiceRoll();
             Console .ReadKey();
         }
     }
--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,10 +8,18 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadder
     {
-        public static void Call()
+        public static void DiceRoll()
         {
             Console.WriteLine("start the snake and ladder Game");
-            Console.WriteLine("Single player at position 0");
+            Console.WriteLine("Press any key to roll the dice");
+            Console.ReadKey();
+
+            // generate a random number between 1 and 6 after dice roll
+            Random random = new Random();
+            int rollDice = random.Next(1, 7);  //used random to get the number between 1 to 6
+
+            Console.WriteLine();
+            Console.WriteLine("You rolled a " + rollDice);
         }
     }
         
